ef678307a3182e5f9c3abebbd881f3b8343d17a3
alyaa999 <alyaamamoon999@gmail.com>
2025-06-10 18:35:37 +0300

    configure httpclient skip generate tests create new enviroment

---
+++ src/app/Services/test-api.service.ts
@@ -0,0 +1,24 @@
+// src/app/services/api.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../enviroment/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl: string = `${environment.apiBaseUrl}/Baskets`;
+
+  constructor(private http: HttpClient) { }
+
+  // GET example
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // POST examples
+  createItem(item: any): Observable<any> {
+    return this.http.post(this.apiUrl, item);
+  }
+}
---
+++ src/enviroment/enviroment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'https://your-api-domain.com/api'
+  };
+  
