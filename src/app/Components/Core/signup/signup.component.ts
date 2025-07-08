3ac97cf32d37df998fd504192738901b779ea38e
abdelrahmanramadan12 <abdelrahmanramadan416@gmail.com>
2025-07-08 23:21:40 +0300

    feat(signup): integrate step3 personal details component into signup flow

--- src/app/Components/Core/signup/signup.component.ts
+++ src/app/Components/Core/signup/signup.component.ts
@@ -4,12 +4,13 @@ import { Step1InitialAccountComponent } from './step1-initial-account/step1-init
 import { Step2ProfileInfoComponent } from "./step2-profile-info/step2-profile-info.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Step3PersonalDetailsComponent } from "./step3-personal-details/step3-personal-details.component";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
-  imports: [Step1InitialAccountComponent, Step2ProfileInfoComponent, CommonModule, FormsModule]
+  imports: [Step1InitialAccountComponent, Step2ProfileInfoComponent, CommonModule, FormsModule, Step3PersonalDetailsComponent]
 })
 export class SignupComponent {
   step = 1;
